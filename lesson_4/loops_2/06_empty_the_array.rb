ad98353cad4c96e309c743c07de5a0945f11202b
Jasmine Galang <jsmn.glng@gmail.com>
2018-03-10 13:15:11 -0800

    Initial commit

---
+++ lesson_4/loops_2/06_empty_the_array.rb
@@ -0,0 +1,11 @@
+# Given the array below, use loop to remove and print each name. Stop the loop
+# once names doesn't contain any more elements.
+
+names = ['Sally', 'Joe', 'Lisa', 'Henry']
+
+# Keep in mind to only use loop, not while, until, etc.
+
+loop do
+  puts names.shift
+  break if names == [] # or names.empty?
+end
