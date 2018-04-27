236415696051cd6b44e31ce508117a609ffc8b30
Jasmine Galang <jsmn.glng@gmail.com>
2018-04-26 19:43:19 -0700

    Added joinor method and keep score feature

--- lesson_6/tic_tac_toe.rb
+++ lesson_6/tic_tac_toe.rb
@@ -1,19 +1,35 @@
 require 'pry'
 
-INITIAL_MARKER = ' '
-PLAYER_MARKER = 'X'
-COMPUTER_MARKER = 'O'
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
                 [[1, 5, 9], [3, 5, 7]]              # diagonals
+
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
 
 def prompt(msg)
   puts "=> #{msg}"
 end
 
+def joinor(arr, delimiter=', ', word='or')
+  case arr.size
+  when 0 then ''
+  when 1 then arr.first
+  when 2 then arr.join(" #{word} ")
+  else
+    arr[-1] = "#{word} #{arr.last}"
+    arr.join(delimiter)
+  end
+end
+
 # rubocop:disable Metrics/AbcSize
-def display_board(brd)
+def display_board(brd, score)
   system 'clear'
+  puts ""
+  puts "#SCORE# Player: #{score['Player']}  Computer: #{score['Computer']}"
+  puts "---------------------------------------------------"
+  puts "Tic-Tac-Toe Game: First to score 5 wins!"
   puts "You're an #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}."
   puts ""
   puts "     |     |"
@@ -44,7 +60,7 @@ end
 def player_places_piece!(brd)
   square = ''
   loop do
-    prompt "Choose a square (#{empty_squares(brd).join(', ')}):"
+    prompt "Choose a square (#{joinor(empty_squares(brd), ', ')}):"
     square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
     prompt "Sorry, that's not a valid choice."
@@ -79,26 +95,46 @@ def detect_winner(brd)
 end
 
 loop do
-  board = intialize_board
+  board = nil
+  score = { 'Player' => 0, 'Computer' => 0 }
 
   loop do
-    display_board(board)
+    board = intialize_board
 
-    player_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
+    loop do
+      display_board(board, score)
 
-    computer_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
+      player_places_piece!(board)
+      break if someone_won?(board) || board_full?(board)
+
+      computer_places_piece!(board)
+      break if someone_won?(board) || board_full?(board)
+    end
+
+    display_board(board, score)
+
+    if someone_won?(board)
+      prompt "#{detect_winner(board)} won!"
+    else
+      prompt "It's a tie!"
+    end
+
+    sleep(1)
+
+    if detect_winner(board) == 'Player'
+      score['Player'] += 1
+    elsif detect_winner(board) == 'Computer'
+      score['Computer'] += 1
+    end
+
+    break if score['Player'] == 5 || score['Computer'] == 5
   end
 
-  display_board(board)
+  display_board(board, score)
 
-  if someone_won?(board)
-    prompt "#{detect_winner(board)} won!"
-  else
-    prompt "It's a tie!"
-  end
-
+  prompt "Grand Winner: #{detect_winner(board)}. Congratulations!"
+  prompt "Player score: #{score['Player']}"
+  prompt "Computer score: #{score['Computer']}"
   prompt "Play again? (y or n)"
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
