2dd73f7c0c551aa65960c300e179977e607c43fe
Jasmine Galang <jsmn.glng@gmail.com>
2018-05-04 10:35:02 -0700

    Initial commit

--- 101-109_small_problems/easy_8/09_convert_number_to_reverse.rb
+++ 101-109_small_problems/easy_8/09_convert_number_to_reverse.rb
@@ -1,32 +1,20 @@
 # Write a method that takes a positive integer as an argument and returns that 
 # number with its digits reversed.
 
-# input: positive int
-# output: reversed type of the input
-# process: turn the input into its reversed type
+def reversed_number(num)
+  str_num = num.to_s
+  arr_num = []
+  counter = str_num.size-1
 
-# given the number called num
-# 1. convert num to string type
-# 2. initialize an empty array called `arr_num`
-# 3. iterate through each character of the string starting from the last element
-#    -convert each num to integer
-#    -place each num to `arr_num`
-# 4. return the value of `arr_num`
+  loop do
+    break if counter < 0
+    char = str_num[counter]
+    arr_num << char
+    counter -= 1
+  end
 
-# def reversed_number(num)
-#   str_num = num.to_s
-#   arr_num = []
-#   counter = str_num.size-1
-
-#   loop do
-#     break if counter < 0
-#     char = str_num[counter]
-#     arr_num << char
-#     counter -= 1
-#   end
-
-#   arr_num.join.to_i
-# end
+  arr_num.join.to_i
+end
 
 # short
 def reversed_number(num)
